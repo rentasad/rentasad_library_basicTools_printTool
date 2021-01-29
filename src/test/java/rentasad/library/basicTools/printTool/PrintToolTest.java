5d376e76f392eb2207a8923ec262ab7eb88ed914
Matthias Staud <matthias.staud@gustini.de>
2021-01-29 23:45:45 +0100

    1.1.0 Remove Eclipse Depencies

--- src/test/java/rentasad/library/basicTools/printTool/PrintToolTest.java
+++ src/test/java/rentasad/library/basicTools/printTool/PrintToolTest.java
@@ -11,7 +11,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import rentasad.library.db.MySQLConnection;
+import rentasad.library.db.MYSQLConnection;
 
 /**
  * 
@@ -43,45 +43,41 @@ public class PrintToolTest
     {
     }
 
-    @Test
-    public void testTestPrint() throws Exception
-    {
-//        printTool.testPrint(); 
-    }
-
-    @Test
-    public void testPrintPdfFile() throws Exception
-    {
-        String filePathToPrint = "c:\\Development\\Workspace New\\gustini.application\\gustini.application.GreetingCardPrinter\\ressources\\unittests\\greetingcards\\todoPath\\430243436.pdf";
-        String printerName = "Godex RT700"; 
-        printTool.printPdfFile(filePathToPrint, printerName);
-    }
 
 
-    @Test
-    public void testPrintInputStream() throws Exception
-    {
-        Map<String, String> mySqlConfigMap = null;
-        PrintTool printTool = new PrintTool();
-        mySqlConfigMap = new HashMap<>();
-        mySqlConfigMap.put("MYSQL_HOST", "gstvmdbs3.gustini.local");
-        mySqlConfigMap.put("MYSQL_DATABASE", "Endkontrolle_DEV1");
-        mySqlConfigMap.put("MYSQL_USER", "greetingcardDev");
-        mySqlConfigMap.put("MYSQL_PASSWORD", "greetingcardDev");
-        mySqlConfigMap.put("MYSQL_ENCODING", "utf8");
-        String printerName = "Godex RT700"; 
-        int cardID = 2639;
-        InputStream is = null;
-        Connection con = MySQLConnection.dbConnect(mySqlConfigMap);
-        String selectQuery = "SELECT pdfFileInputStreamContent FROM greetingCards g WHERE g.cardID=?";
-        PreparedStatement ps = con.prepareStatement(selectQuery);
-        ps.setInt(1, cardID);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next())
-        {
-            is = rs.getBinaryStream("pdfFileInputStreamContent");
-        }
-        printTool.printInputStream(is, printerName);
-        con.close();
-    }
+//    @Test
+//    public void testPrintPdfFile() throws Exception
+//    {
+//        String filePathToPrint = "c:\\Development\\Workspace New\\gustini.application\\gustini.application.GreetingCardPrinter\\ressources\\unittests\\greetingcards\\todoPath\\430243436.pdf";
+//        String printerName = "Godex RT700"; 
+//        printTool.printPdfFile(filePathToPrint, printerName);
+//    }
+
+
+//    @Test
+//    public void testPrintInputStream() throws Exception
+//    {
+//        Map<String, String> mySqlConfigMap = null;
+//        PrintTool printTool = new PrintTool();
+//        mySqlConfigMap = new HashMap<>();
+//        mySqlConfigMap.put("MYSQL_HOST", "gstvmdbs3.gustini.local");
+//        mySqlConfigMap.put("MYSQL_DATABASE", "Endkontrolle_DEV1");
+//        mySqlConfigMap.put("MYSQL_USER", "greetingcardDev");
+//        mySqlConfigMap.put("MYSQL_PASSWORD", "greetingcardDev");
+//        mySqlConfigMap.put("MYSQL_ENCODING", "utf8");
+//        String printerName = "Godex RT700"; 
+//        int cardID = 2639;
+//        InputStream is = null;
+//        Connection con = MYSQLConnection.dbConnect(mySqlConfigMap);
+//        String selectQuery = "SELECT pdfFileInputStreamContent FROM greetingCards g WHERE g.cardID=?";
+//        PreparedStatement ps = con.prepareStatement(selectQuery);
+//        ps.setInt(1, cardID);
+//        ResultSet rs = ps.executeQuery();
+//        if (rs.next())
+//        {
+//            is = rs.getBinaryStream("pdfFileInputStreamContent");
+//        }
+//        printTool.printInputStream(is, printerName);
+//        con.close();
+//    }
 }
